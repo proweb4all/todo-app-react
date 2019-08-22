0e4a9a2ba6641916c2513e14f2491a84c4b764b0
proweb4all <proweb4all@narod.ru>
2019-08-22 14:57:36 +0300

    addTask

--- src/containers/todo/todo.jsx
+++ src/containers/todo/todo.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { addTask } from '../../actions/actionCreator';
 
 import ToDoInput from '../../components/todo-input/todo-input';
 import ToDoList from '../../components/todo-list/todo-list';
@@ -6,43 +9,45 @@ import Footer from '../../components/footer/footer';
 
 import './todo.css';
 
-const TASKS = [
-  {
-    id: 1,
-    text: 'Learn ReactJS',
-    isCompleted: true,
-  },
-  {
-    id: 2,
-    text: 'Learn Redux',
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    text: 'Learn React Router',
-    isCompleted: false,
-  }
-];
-
 class ToDo extends Component {
 
   state = {
     activeFilter: 'all',
+    taskText: ''
+  }
+
+  handleInputChange = ({ target: { value } }) => {
+    this.setState({
+      taskText: value
+    })
+  }
+
+  addTask = ({ key }) => {
+    const { taskText } = this.state;
+    if ( taskText.length > 3 && key === 'Enter' ) {
+      const { addTask } = this.props;
+      addTask((new Date()).getTime(), taskText, false);
+      this.setState({
+        taskText: '',
+      })
+    }
   }
 
   render() {
-    const { activeFilter } = this.state;
-    const tasksList = TASKS;
-    const isTasksExist = tasksList && tasksList.length > 0;
+    const { activeFilter, taskText } = this.state;
+    const { tasks } = this.props;
+    const isTasksExist = tasks && tasks.length > 0;
 
     return (
       <div className="todo-wrapper">
-        <ToDoInput />
-        {isTasksExist && <ToDoList tasksList={tasksList} />}
-        {isTasksExist && <Footer amount={tasksList.length} activeFilter={activeFilter} />}
+        <ToDoInput onKeyPress={this.addTask} onChange={this.handleInputChange} value={taskText} />
+        {isTasksExist && <ToDoList tasksList={tasks} />}
+        {isTasksExist && <Footer amount={tasks.length} activeFilter={activeFilter} />}
       </div>
     );
   }
 }
 
-export default ToDo;
+export default connect(state => ({
+  tasks: state.tasks,
+}), { addTask })(ToDo);
